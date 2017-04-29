7c8a4f4d99acfb3e1b27edbad5ab15196e15cfcc
Mike Holm <mholm@rallydev.com>
2017-04-29 11:36:32 -0600

    flip logic, remove filter

--- app.js
+++ app.js
@@ -4,93 +4,81 @@ window.onload = function() {
 };
 
 function renderState(state) {
-  if(!state) {
-    console.log('no state to render');
-    return "";
+  if(state) {
+    console.log('rendering state', state);
+    return renderBoard(state.board);
   }
-
-  console.log('rendering state', state);
-  return renderBoard(state.board);
+  return "";
 }
 
 function renderBoard(board) {
-  if(!board) {
-    console.log('no board to render');
-    return "";
+  if(board) {
+    console.log('rendering board', board);
+    const b = `
+      <div class="board" id="board|${board.id}">
+        ${renderColumns(board.columns, board.cards)}
+      </div>
+    `;
+    console.log('board', b);
+    return b;
   }
 
-  console.log('rendering board', board);
-  const b = `
-<div class="board" id="board|${board.id}">
-  ${renderColumns(board.columns, board.cards)}
-</div>
-`;
-  console.log('board', b);
-  return b;
+  return "";
 }
 
 function renderColumns(columns, cards) {
-  if(!columns) {
-    console.log('no columns to render');
-    return "";
+  if(columns) {
+    console.log('rendering columns', columns);
+    return columns.reduce((acc, column) => {
+      function cardBelongsInColumn(card) { return card.column === column.id; }
+      const cardsInColumn = cards.filter(cardBelongsInColumn);
+      return acc += renderColumn(column, cardsInColumn);
+    }, "");
   }
-
-  console.log('rendering columns', columns);
-  return columns.reduce((acc, column) => {
-    function cardBelongsInColumn(card) { return card.column === column.id; }
-    return acc += renderColumn(column, filter(cards, cardBelongsInColumn));
-  }, "");
-}
-
-function filter(arr, pred) {
-  return arr.reduce((acc, cur) => {
-    return pred(cur) ? acc.concat([cur]) : acc;
-  }, []);
+  return "";
 }
 
 function renderColumn(column, cards) {
-  if(!column) {
-    console.log('no column to render');
-    return "";
+  if(column) {
+    console.log('rendering column', column);
+    const c = `
+      <div class="column" id="col|${column.id}">
+        ${column.name}
+        ${renderCards(cards)}
+      </div>
+    `;
+    console.log('column', c);
+    return c;
   }
-
-  console.log('rendering column', column);
-  const c = `
-<div class="column" id="col|${column.id}">
-  ${column.name}
-  ${renderCards(cards)}
-</div>
-`;
-  console.log('column', c);
-  return c;
+  return "";
 }
 
 function renderCards(cards) {
-  if(!cards) {
-    console.log('no cards to render');
-    return "";
+  if(cards) {
+    console.log('rendering cards', cards);
+    return cards.reduce((acc, card) => {
+      return acc += renderCard(card);
+    }, "");
   }
-
-  console.log('rendering cards', cards);
-  return cards.reduce((acc, card) => {
-    return acc += renderCard(card);
-  }, "");
+  return "";
 }
 
 function renderCard(card) {
-  if(!card) {
-    console.log('no card to render');
-    return "";
+  if(card) {
+    console.log('rendering card', card);
+    const c = `
+      <div class="card" id="card|${card.id}" onclick="foo()">
+        ${card.name}
+      </div>
+    `;
+    console.log('card', c);
+    return c;
   }
+  return "";
+}
 
-  console.log('rendering card', card);
-  const c = `
-<div class="card" id="card|${card.id}">
-  ${card.name}
-</div>
-`;
-  console.log('card', c);
-  return c;
+function foo() {
+  console.log();
 }
 
 function getInitialState() {
